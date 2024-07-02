777fc5725bd0f86626723565e02b71729cb9e820
Ashan Dimantha <ashandimanthalk@gmail.com>
2024-07-02 20:33:23 +0530

    add: numbers

--- src/Components/Numbers.jsx
+++ src/Components/Numbers.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import numberSlice, {
   decrement,
@@ -9,30 +9,31 @@ import numberSlice, {
 export default function Numbers() {
   const dispatch = useDispatch();
   const data = useSelector((state) => state.numbers.number);
+  const inputRef = useRef(null);
+  const subtract = () => {
+    dispatch(decrement());
+  };
+
+  const add = () => {
+    dispatch(increment());
+  };
+
+  const addbyValue = () => {
+    if (!inputRef.current.value) {
+      alert("Input a number");
+    } else {
+      const value = Number(inputRef.current.value);
+      dispatch(incrementByAmount(value));
+    }
+  };
+
   return (
     <div>
       <h1>Counter: {data}</h1>
-      <button
-        onClick={() => {
-          dispatch(increment());
-        }}
-      >
-        Increment
-      </button>
-      <button
-        onClick={() => {
-          dispatch(decrement());
-        }}
-      >
-        Decerement
-      </button>
-      <button
-        onClick={() => {
-          dispatch(incrementByAmount(7));
-        }}
-      >
-        Increment by 7
-      </button>
+      <button onClick={add}>Increment</button>
+      <button onClick={subtract}>Decerement</button>
+      <input type="number" ref={inputRef} />
+      <button onClick={addbyValue}>Increment by Amount</button>
       <br />
       <br />
       <br />
